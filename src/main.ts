e2e18a2c68891b70014b4a99b5ddeeb05581672c
xsour <you@example.com>
2024-11-18 11:12:46 +0200

    last change

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './index';
+import router from './router';
 import { createPinia } from 'pinia';
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 import './assets/tailwind.css';
